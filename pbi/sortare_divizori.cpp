95964c70764f4ed2bf20c237fe4ee280aeca6972
Radulescu <andrei.radulescu006@gmail.com>
2022-04-28 21:52:59 +0300

    enter

--- pbi/sortare_divizori.cpp
+++ pbi/sortare_divizori.cpp
@@ -6,50 +6,52 @@ using namespace std;
 ifstream cin("sortare_divizori.in");
 ofstream cout("sortare_divizori.out");
 
-struct pereche{
+struct pereche
+{
     int numar;
     int numardiv;
-}numar[1001];
+} numar[1001];
 
 int numar_divizori(int n)
 {
-    int nrd=0;
-    for(int i=1;i<=sqrt(n);i++)
+    int nrd = 0;
+    for (int i = 1; i <= sqrt(n); i++)
     {
-        if(n%i==0)
+        if (n % i == 0)
         {
-            if(n/i==i)
+            if (n / i == i)
                 nrd++;
-            else nrd+=2;
+            else
+                nrd += 2;
         }
     }
     return nrd;
 }
 
-void quicksort(pereche v[],int st,int dr)
+void quicksort(pereche v[], int st, int dr)
 {
-    if(st<dr)
+    if (st < dr)
     {
-        int m=(st+dr)/2;
-        swap(v[m],v[st]);
-        int i=st,j=dr,d=0;
-        while(i<j)
+        int m = (st + dr) / 2;
+        swap(v[m], v[st]);
+        int i = st, j = dr, d = 0;
+        while (i < j)
         {
-            if(v[i].numardiv<v[j].numardiv)
+            if (v[i].numardiv < v[j].numardiv)
             {
-                swap(v[i],v[j]);
-                d=1-d;
+                swap(v[i], v[j]);
+                d = 1 - d;
             }
-            else if(v[i].numardiv==v[j].numardiv && v[i].numar>v[j].numar)
+            else if (v[i].numardiv == v[j].numardiv && v[i].numar > v[j].numar)
             {
-                swap(v[i],v[j]);
-                d=1-d;
+                swap(v[i], v[j]);
+                d = 1 - d;
             }
-            i+=d;
-            j-=1-d;
+            i += d;
+            j -= 1 - d;
         }
-        quicksort(v,st,i-1);
-        quicksort(v,i+1,dr);
+        quicksort(v, st, i - 1);
+        quicksort(v, i + 1, dr);
     }
 }
 
@@ -57,13 +59,13 @@ int n;
 
 int main(void)
 {
-    cin>>n;
-    for(int i=1;i<=n;i++)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        cin>>numar[i].numar;
-        numar[i].numardiv=numar_divizori(numar[i].numar);
+        cin >> numar[i].numar;
+        numar[i].numardiv = numar_divizori(numar[i].numar);
     }
-    quicksort(numar,1,n);
-    for(int i=1;i<=n;i++)
-        cout<<numar[i].numar<<" ";
+    quicksort(numar, 1, n);
+    for (int i = 1; i <= n; i++)
+        cout << numar[i].numar << " ";
 }
